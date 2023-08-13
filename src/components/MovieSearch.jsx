ace0b371f9336f7c042a12131fb55f9309e75e26
aytmamatov_musa <musaaytmamatov@gmail.com>
2023-08-13 19:30:18 +0600

    Added filters by type

--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -5,10 +5,19 @@ import { Link } from 'react-router-dom';
 function MovieSearch() {
   const [searchValue, setSearchValue] = useState('');
   const [movies, setMovies] = useState([]);
+  const [filterType, setFilterType] = useState('movie');
 
-  async function handleSearch() {
+  async function handleSearch(event) {
     try {
-      const { data: { Search } } = await httpService.get('/', { params: { s: searchValue } })
+      event.preventDefault()
+      const { data: { Search } } = await httpService.get(
+        '/',
+        {
+          params: {
+            s: searchValue,
+            type: filterType,
+          }
+        })
       setMovies(Search);
     } catch (error) {
       console.error("Error fetching movies")
@@ -18,7 +27,7 @@ function MovieSearch() {
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center justify-center p-4">
       <h1 className="text-3xl font-bold mb-4">Movie Search</h1>
-      <div className="flex">
+      <form className="flex">
         <input
           type="text"
           className="border rounded-l py-2 px-4 w-64 focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -26,17 +35,25 @@ function MovieSearch() {
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
         />
+        <select
+          value={filterType}
+          onChange={(e) => setFilterType(e.target.value)}
+          className="border mx-2 rounded-l py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          <option value="movie">Movie</option>
+          <option value="series">Series</option>
+        </select>
         <button
           className="bg-blue-500 text-white py-2 px-4 rounded-r"
           onClick={handleSearch}
         >
           Search
         </button>
-      </div>
+      </form>
       <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
         {movies.map((movie) => (
           <li key={movie.imdbID} className="bg-white rounded-lg shadow-md p-4">
-            <Link to={`movie/${movie.imdbID}`}>
+            <Link to={`movie/${movie.imdbID}`} target="_blank">
               <img
                 src={movie.Poster}
                 alt={`${movie.Title} Poster`}
